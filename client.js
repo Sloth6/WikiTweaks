41fb5b84468da9cada1e48307ffe1b6ef5575a39
Antonio Ono <me@antonioono.com>
2014-12-17 21:18:12 -0500

    switched to custom js file

--- client.js
+++ client.js
@@ -1,20 +1,26 @@
+var search;
+
 $("document").ready(function() {
+    var search = $('#p-search');
     reStylePage();
 });
 
 function reStylePage() {
-//     $('head link, head style').remove();
+    $('head link, head style').remove();
     $('#siteSub,#contentSub,.metadata').remove();
     $('#mw-head,#mw-page-base,#mw-panel').hide();
-    $('#firstHeading').append(search)
+    $('#firstHeading').append(search);
     $('#content').css('margin-left', 0);
     $('#simpleSearch').css({
-        'padding-right': 20,
-        'width': 100,
-        'margin-top': 5,
-        'margin-bottom': 5
+    'padding-right': 20,
+    'width': 100,
+    'margin-top': 5,
+    'margin-bottom': 5
     });
     $('#content').css({
-        'padding-top':0
+    'padding-top':0
     });
+    
+    var style = $('<link rel="stylesheet" href="./style/screen.css">');
+    $("head").append(style);
 }
